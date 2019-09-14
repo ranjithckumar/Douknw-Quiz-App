f6001d7403825e9e8ba714ef1cdc5c5748eff365
Gokul <gokulramalingam1@gmail.com>
2019-09-14 15:45:20 +0530

    update in result page

--- result.js
+++ result.js
@@ -1,27 +1,42 @@
 $("document").ready(function(){
     let score = 0;
+    let htmlScore = 0;
+    let cssScore = 0;
+    let jsScore = 0;
     for(let i = 1;i<=10;i++){
         let o = JSON.parse(localStorage.getItem(i));
         let ans = localStorage.getItem("a"+i);
         
        if(ans != null && o["answer"]===ans)
+       {
           score++;
+          htmlScore++;
+       }
     }
     for(let i = 11;i<=20;i++){
         let o = JSON.parse(localStorage.getItem(i));
         let ans = localStorage.getItem("a"+i);
        
        if(ans != null && o["answer"]===ans)
+       {
           score++;
+          cssScore++;
+       }
     }
     for(let i = 21;i<=30;i++){
         let o = JSON.parse(localStorage.getItem(i));
         let ans = localStorage.getItem("a"+i);
        
        if(ans != null && o["answer"]===ans)
+       {
           score++;
+          jsScore++;
+       }
     }
     $("#marks").text(score);
+    $("#htmlScoreCard").text(htmlScore);
+    $("#cssScoreCard").text(cssScore);
+    $("#jsScoreCard").text(jsScore);
     localStorage.clear();
 })
 
