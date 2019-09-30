21e1cbb115de4fdb2efbf4bb6e1389863fdcfe00
Gokul <gokulramalingam1@gmail.com>
2019-09-30 16:40:13 +0530

    update in questions page

--- question.js
+++ question.js
@@ -19,6 +19,9 @@ let timer = ()=>{
    location.href="section.html";
 }, 1000);
 }
+
+let answerArray = [];
+let selectedOptions = [];
 let questions;
 let query = window.location.search.substring(1,);
 
@@ -36,17 +39,6 @@ else
  }
 console.log(questions);
 
-// if(localStorage.getItem("sec") == 1){
-// let firstQuestion = JSON.parse(localStorage.getItem(1));
-// $("#question").text(firstQuestion["question"]);
-// $("#opt1").text(firstQuestion["options"][0]);
-// $("#opt2").text(firstQuestion["options"][1]);
-// $("#opt3").text(firstQuestion["options"][2]);
-// $("#opt4").text(firstQuestion["options"][3]);
-
-
-
-
 $(document).ready(()=>{
 
 $('#img-forward').click(()=>{
@@ -58,41 +50,28 @@ $('#img-forward').click(()=>{
 
 let questionNumber = 0;
 $("#img-forward").click(()=>{
-
-        
-        $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
-    //    console.log(key);
-    //    console.log();
+        // console.log(localStorage.hasOwnProperty(questionNumber));
+       
        if(questionNumber < 10)
-       action('add');
-        // $("#num").text(questionNumber);
-        // $("#question").text(questions[questionNumber-1][key].question);
-        // $("#opt1").text(requiredQuestion[0]["options"][0]);
-        // $("#opt2").text(requiredQuestion[0]["options"][1]);
-        // $("#opt3").text(requiredQuestion[0]["options"][2]);
-        // $("#opt4").text(requiredQuestion[0]["options"][3]);
-        // questionNumber++;  
-
+       {
+        $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
+        action('add');
+       if(selectedOptions[questionNumber-1]!=undefined) 
+       $("#option"+selectedOptions[questionNumber-1]).css('background-color','#78909C');
+       }
 })
 
 
 $("#img-backward").click(()=>{
     if(questionNumber > 1)
-    action('sub');
-//     if(question>1)
-//     {
-//         question--;
-//         questionNumber--;
-//         $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
-//         let requiredQuestion = JSON.parse(localStorage.getItem(question));
-//         console.log(requiredQuestion)
-//         $("#num").text(questionNumber);
-//         $("#question").text(requiredQuestion["question"]);
-//         $("#opt1").text(requiredQuestion["options"][0]);
-//         $("#opt2").text(requiredQuestion["options"][1]);
-//         $("#opt3").text(requiredQuestion["options"][2]);
-//         $("#opt4").text(requiredQuestion["options"][3]);
-//     }
+    {
+        $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
+        action('sub');
+       if(selectedOptions[questionNumber-1]!=undefined)
+        $("#option"+selectedOptions[questionNumber-1]).css('background-color','#78909C') ;
+      
+       
+    }
 })
 
 let action =((option)=>{
@@ -100,8 +79,6 @@ let action =((option)=>{
 if(questionNumber>=1 && questionNumber <= 10)
 {
     key = Object.keys(questions[questionNumber-1])[1];
-    console.log(key);
-    console.log();
     $("#num").text(questionNumber);
     $("#question").text(questions[questionNumber-1][key].question);
     $("#opt1").text(questions[questionNumber-1][key]["options"][0]);
@@ -111,128 +88,6 @@ if(questionNumber>=1 && questionNumber <= 10)
 }
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-// timer();
-// }
-
-// if(localStorage.getItem("sec") == 2){
-//     $("#section-number").text("Section 2");
-//     question = 11;
-//     let firstQuestion = JSON.parse(localStorage.getItem(11));
-//     $("#question").text(firstQuestion["question"]);
-//     $("#opt1").text(firstQuestion["options"][0]);
-//     $("#opt2").text(firstQuestion["options"][1]);
-//     $("#opt3").text(firstQuestion["options"][2]);
-//     $("#opt4").text(firstQuestion["options"][3]);
-    
-    
-//     $("#img-backward").click(function(){
-//         if(question>11)
-//         {
-//             question--;
-//             questionNumber--;
-//             $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
-//             let requiredQuestion = JSON.parse(localStorage.getItem(question));
-//             console.log(requiredQuestion)
-//             $("#num").text(questionNumber);
-//             $("#question").text(requiredQuestion["question"]);
-//             $("#opt1").text(requiredQuestion["options"][0]);
-//             $("#opt2").text(requiredQuestion["options"][1]);
-//             $("#opt3").text(requiredQuestion["options"][2]);
-//             $("#opt4").text(requiredQuestion["options"][3]);
-            
-//         }
-//     })
-    
-    
-//     $("#img-forward").click(function(){
-//         if(question != 20)
-//         {
-//             $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
-//             // localStorage.setItem("a"+question,answer);
-//             question++;
-//             questionNumber++;
-//             let requiredQuestion = JSON.parse(localStorage.getItem(question));
-//             // console.log(requiredQuestion)
-            
-//             $("#num").text(questionNumber);
-            
-//             $("#question").text(requiredQuestion["question"]);
-//             $("#opt1").text(requiredQuestion["options"][0]);
-//             $("#opt2").text(requiredQuestion["options"][1]);
-//             $("#opt3").text(requiredQuestion["options"][2]);
-//             $("#opt4").text(requiredQuestion["options"][3]);
-            
-//         }
-
-//     })
-//     timer();
-//     }
-    
-
-    
-//     if(localStorage.getItem("sec") == 3){
-//         $("#section-number").text("Section 3");
-//         question = 21;
-//     let firstQuestion = JSON.parse(localStorage.getItem(21));
-//     $("#question").text(firstQuestion["question"]);
-//     $("#opt1").text(firstQuestion["options"][0]);
-//     $("#opt2").text(firstQuestion["options"][1]);
-//     $("#opt3").text(firstQuestion["options"][2]);
-//     $("#opt4").text(firstQuestion["options"][3]);
-    
-   
-//     $("#img-backward").click(function(){
-//         if(question>21)
-//         {
-//             question--;
-//             questionNumber--;
-//             $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
-//             let requiredQuestion = JSON.parse(localStorage.getItem(question));
-//             console.log(requiredQuestion)
-//             $("#num").text(questionNumber);
-//             $("#question").text(requiredQuestion["question"]);
-//             $("#opt1").text(requiredQuestion["options"][0]);
-//             $("#opt2").text(requiredQuestion["options"][1]);
-//             $("#opt3").text(requiredQuestion["options"][2]);
-//             $("#opt4").text(requiredQuestion["options"][3]);
-            
-//         }
-//     })
-    
-//     $("#img-forward").click(function(){
-//         if(question != 30)
-//         {
-//             $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
-//             // localStorage.setItem("a"+question,answer);
-//             question++;
-//             questionNumber++;
-//             let requiredQuestion = JSON.parse(localStorage.getItem(question));
-//             // console.log(requiredQuestion)
-//             $("#num").text(questionNumber);
-//             $("#question").text(requiredQuestion["question"]);
-//             $("#opt1").text(requiredQuestion["options"][0]);
-//             $("#opt2").text(requiredQuestion["options"][1]);
-//             $("#opt3").text(requiredQuestion["options"][2]);
-//             $("#opt4").text(requiredQuestion["options"][3]);
-            
-//         }
-//     })
-//     timer();
-//     }
-    
-
  $("#option1").click(function(){
   selector(1);
   $("#option2, #option3, #option4").css('background-color','#CFD8DC');
@@ -240,37 +95,31 @@ if(questionNumber>=1 && questionNumber <= 10)
 
  $("#option2").click(function(){
     selector(2);
-    // localStorage.setItem("a"+question,answer);
-    // $("#option2").css('background-color','#78909C');
     $("#option1, #option3, #option4").css('background-color','#CFD8DC');
    })
 
    $("#option3").click(function(){
     selector(3);
-    // localStorage.setItem("a"+question,answer);
-    // $("#option3").css('background-color','#78909C');
     $("#option1, #option2, #option4").css('background-color','#CFD8DC');
    })
 
    $("#option4").click(function(){
     selector(4);
-    // localStorage.setItem("a"+question,answer);
-    // $("#option4").css('background-color','#78909C');
     $("#option1, #option2, #option3").css('background-color','#CFD8DC');
    })
-let answerArray = [];
+
 let selector = ((option)=>{
     answerArray[questionNumber-1] = $("#opt"+option).text();
-    console.log(answerArray);
+    selectedOptions[questionNumber-1]=option;
     $("#option"+option).css('background-color','#78909C');
 })
 
 
-//    $("#submitButton").click(function(){
-//        location.href = 'result.html'
-//    })
+   $("#submitButton").click(function(){
+       location.href = 'result.html'
+   })
 
-//    $("#sectionButton").click(function(){
-//        location.href = 'section.html'
-//    })
+   $("#sectionButton").click(function(){
+       location.href = 'section.html'
+   })
  
