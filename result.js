5cbbdb1b6ce78ca87a4671dbf4988185946f60f9
Gokul <gokulramalingam1@gmail.com>
2019-10-01 14:42:38 +0530

    Updation in result page

--- result.js
+++ result.js
@@ -8,11 +8,11 @@ $(document).ready(function(){
         type: 'GET',
         success:function(data){
            htmlScore =  calculator(data,"html");
-           console.log(htmlScore);
+          
            cssScore = calculator(data,"css");
-           console.log(cssScore);
+          
            jsScore = calculator(data,'js');
-           console.log(jsScore);
+         
            score = htmlScore+cssScore+jsScore;
            $("#marks").text(score);
            $("#htmlScoreCard").text(htmlScore);
@@ -21,8 +21,11 @@ $(document).ready(function(){
         }
     })
  
+ 
   });
-//   key = Object.keys(json[0])[1];
+
+
+
 let calculator = (json,type) => {
   let  answers = JSON.parse(localStorage.getItem(type+"Answers"));
   let keys = [];
@@ -39,24 +42,22 @@ let questions = json.filter(data=>{
     return  data[Object.keys(data)[1]]["type"] === type;
 } )
 matches = questions.filter(hk=>{
-//    console.log(Object.keys(htmlquestions[0][1]));
+
 if(keys.includes(Object.keys(hk)[1]))
 return  hk;
 }).map(hk=>{return  {key:Object.keys(hk)[1],
      answer:hk[Object.keys(hk)[1]]['answer']}})
-console.log(answers)
-console.log(matches);
+
 var score = 0;
 answers.forEach(hA=>
     {
-        console.log(hA.key);
+
        matcher =  matches.filter(hM=>hA.key === hM.key)
        if(hA.answer === matcher[0].answer)
        score += 1;
     })
     return score;
-    //     $("#htmlScoreCard").text(htmlScore);
-    // console.log(htmlScore);
+
 }
 return 0;
 }
