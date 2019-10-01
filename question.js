762af7243ba32db878c66973bf70eb052a6275e0
Gokul <gokulramalingam1@gmail.com>
2019-10-01 17:13:49 +0530

    Addition of commands

--- question.js
+++ question.js
@@ -15,8 +15,8 @@ let timer = ()=>{
    //minutes = (minutes < 10) ?  minutes : minutes;
    $('#time').html(minutes + ':' + seconds);
    timer2 = minutes + ':' + seconds;
-   if(minutes===0&&seconds==="00")
-   location.href="section.html";
+   if(minutes===0&&seconds==="00")      
+   location.href="section.html";               //If time reaches 0:00 taking back user to section page
 }, 1000);
 }
 
@@ -30,23 +30,23 @@ let query = window.location.search.substring(1,); // ==> html
 
 if(query === 'html')
 {
-questions = JSON.parse( localStorage.getItem("htmlArray"));
-localStorage.removeItem('htmlArray');
+questions = JSON.parse( localStorage.getItem("htmlArray")); //Getting an array from local storage which is populated with 10 html questions
+localStorage.removeItem('htmlArray');//Removing it from local storage
 }
 else if(query === 'css')
 {
-    questions = JSON.parse(localStorage.getItem("cssArray"));
-    localStorage.removeItem('cssArray');
+    questions = JSON.parse(localStorage.getItem("cssArray"));//Getting an array from local storage which is populated with 10 html questions
+    localStorage.removeItem('cssArray');//Removing it from local storage
 }
 else
  {
-   questions = JSON.parse(localStorage.getItem("jsArray"));
-   localStorage.removeItem('jsArray');
+   questions = JSON.parse(localStorage.getItem("jsArray"));   //Getting an array from local storage which is populated with 10 html questions
+   localStorage.removeItem('jsArray');   //Removing it from local storage
  }
 
 
 $(document).ready(()=>{
-
+//To generate first question an automatic click is made to avoid conflicts
 $('#img-forward').click(()=>{
  });
  $('#img-forward').click();
@@ -56,19 +56,22 @@ $('#img-forward').click(()=>{
 
 let questionNumber = 0;
 $("#img-forward").click(()=>{
-        // console.log(localStorage.hasOwnProperty(questionNumber));
+        // This will protect next question does not exceed beyond the limit of 10 questions
        
        if(questionNumber < 10)
        {
-        $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
-        action('add');
-       if(selectedOptions[questionNumber-1]!=undefined) 
-       $("#option"+selectedOptions[questionNumber-1]).css('background-color','#78909C');
+        $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC'); //As soon as a new question gets loaded all the options background are given same background color
+        action('add'); // Here a function is being called and passed a parameter 'add' so that using that argument next question will get incremented
+        //The below if statement checks whether selectedOption array has any value in the particular index based on the questionnumber.  
+        if(selectedOptions[questionNumber-1]!=undefined) 
+       $("#option"+selectedOptions[questionNumber-1]).css('background-color','#78909C');//If that array have any option among 1,2,3&4 that particular option will get highlighted
        }
 })
 
 
 $("#img-backward").click(()=>{
+
+    //Samething happens for this but in the opposite manner compared to img-forward.It check whether question doesn't exeed below 1;
     if(questionNumber > 1)
     {
         $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
@@ -78,6 +81,8 @@ $("#img-backward").click(()=>{
     }
 })
 let key;
+
+//In this function 'add' or 'sub' is passed as parameter using that argument function increments the question or decrements the question from array of 10 questions.
 let action =((option)=>{
  option==='add'?questionNumber++:questionNumber--;
 if(questionNumber>=1 && questionNumber <= 10)
@@ -91,7 +96,7 @@ if(questionNumber>=1 && questionNumber <= 10)
     $("#opt4").text(questions[questionNumber-1][key]["options"][3]);
 }
 })
-
+//When the user clicks on particular option that particular option gets highlighted and remaining options are changed to default color to avoid conflicts.
  $("#option1").click(()=>{
   selector(1);
   $("#option2, #option3, #option4").css('background-color','#CFD8DC');
@@ -111,7 +116,7 @@ if(questionNumber>=1 && questionNumber <= 10)
     selector(4);
     $("#option1, #option2, #option3").css('background-color','#CFD8DC');
    })
-
+//Selector stores the question number along with answer which was answer by user in local storage in form of array of objects.
 let selector = ((option)=>{
     answerArray[questionNumber-1] ={key:key,answer : $("#opt"+option).text()};
     selectedOptions[questionNumber-1]=option;
